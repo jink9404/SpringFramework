b37b0ab999e04c6f611b734c9024dccba33f5d77
MJ <shinyjade94@gmail.com>
2020-05-21 15:19:16 +0900

    어노테이션 설명 작성

--- aDI/src/ex06_autowiring/MessageBeanImpl.java
+++ aDI/src/ex06_autowiring/MessageBeanImpl.java
@@ -1,5 +1,7 @@
 package ex06_autowiring;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -8,8 +10,9 @@ public class MessageBeanImpl implements MessageBean {
 	private String name;
 	private String message;
 	
-	@Autowired
-	@Qualifier("outputer2")
+//	@Autowired			//생성자나 setter가 없이 DI(주입)해 주는 어노테이션
+//	@Qualifier("outputer2")//동일한 클래스의 빈이 여러개인 경우 선택하는 어노테이션
+	@Resource(name = "outputer2") // = @Autowired + @Qualifier("outputer2")
 	private Outputer outputer;
 	
 	public void setName(String name) {
