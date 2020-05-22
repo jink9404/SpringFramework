a3f0a23e53f9f7c1987bcd5173b731d1dd01c6b4
MJ <shinyjade94@gmail.com>
2020-05-22 10:34:07 +0900

    2020-05-22 10:33 AOP

---
+++ cAOP/src/MainApp.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import app1_xml.LoggingAdvice;
+import app1_xml.MessageBean;
+
+public class MainApp {
+	public static void main(String[] args) {
+//		MessageBean 구현한 클래스의 모든 메소드를 호출
+		ApplicationContext context = new GenericXmlApplicationContext("app1_xml/applicationContext.xml");
+		MessageBean bean = context.getBean("targetBean", MessageBean.class);
+		bean.engSayHello();
+	
+		System.out.println("-----------------------------------------------------------------");
+		bean.sayHello();
+		System.out.println("-----------------------------------------------------------------");
+		bean.test();
+		System.out.println("-----------------------------------------------------------------");
+	}
+}
---
+++ cAOP/src/app1_xml/MessageBean.java
@@ -0,0 +1,7 @@
+package app1_xml;
+
+public interface MessageBean {
+	public abstract void sayHello();
+	public abstract void engSayHello();
+	public abstract void test();
+}
