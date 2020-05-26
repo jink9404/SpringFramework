4fb7dc4c02370efcba1c9c1602212f1d169127b6
MJ <shinyjade94@gmail.com>
2020-05-26 10:23:19 +0900

    SpringMVC redirect  2020-05-26 10:23 by MJ

---
+++ fSpringMVC/src/main/java/com/kosmo/controller/RedirectController.java
@@ -0,0 +1,23 @@
+package com.kosmo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RedirectController {
+
+	@RequestMapping(value = "/select.do")
+	public ModelAndView select() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("redirectResult");
+		mv.addObject("info","여기는 목록 보기입니다");
+		return mv;
+	}
+	@RequestMapping(value = "/insert.do")
+	public String insert() {
+		System.out.println("실제로 DB에 입력됨");
+		return "redirect:select.do";
+	}
+}
