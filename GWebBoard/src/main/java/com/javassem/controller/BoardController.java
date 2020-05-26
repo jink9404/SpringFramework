9fe82127b51e56b2e70c5ce22ec8c19f976d2ef6
MJ <shinyjade94@gmail.com>
2020-05-26 14:39:19 +0900

    UPDATE 2020-05-26 14:38 BY MJ

--- GWebBoard/src/main/java/com/javassem/controller/BoardController.java
+++ GWebBoard/src/main/java/com/javassem/controller/BoardController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javassem.domain.BoardVO;
@@ -21,7 +21,38 @@ public class BoardController {
 		//검색 기능 때문에 BoardVO를 지정
 		List<BoardVO> list = service.getBoardList(vo);
 		model.addAttribute("boardList", list);
+	}// view page : getBoardList.jsp
+	
+	
+	@RequestMapping(value = "{step}.do")
+	public String changePage(@PathVariable String step) {return step;}
+	//화면만 전환하기위해서 생성함. 하는일 없는 함수를 생성하는것을 막기위해
+	
+	//@RequestMapping(value = "/insertBoard.do")
+	//public void insertBoard() {	}	
+	//view page : insertBoard.jsp
+
+	@RequestMapping(value = "/saveBoard.do")
+	public String saveBoard(BoardVO vo) {
+		service.insertBoard(vo);
+		return "redirect:getBoardList.do";
+	}//view page : boardList.jsp
+	//redirect : getBoardList.do
+	
+	@RequestMapping(value = "/getBoard.do")
+	public void getBoard(BoardVO vo,Model m) {
+		m.addAttribute("board", service.getBoard(vo));
 	}
-
-
+	
+	@RequestMapping(value = "/deleteBoard.do")
+	public String deleteBoard(BoardVO vo) {
+		service.deleteBoard(vo);
+		return "redirect:getBoardList.do";
+	}
+	
+	@RequestMapping(value = "/updateBoard.do")
+	public String updateBoard(BoardVO vo) {
+		service.updateBoard(vo);
+		return "redirect:getBoardList.do";
+	}
 }
