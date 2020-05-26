33b04c492c759316fb7e9594cbb54ae7ccc300b9
MJ <shinyjade94@gmail.com>
2020-05-26 12:03:28 +0900

    SpringMVC 2020-05-26 12:02 by MJ

--- GWebBoard/src/test/java/com/javassem/basic/MybatisTest.java
+++ GWebBoard/src/test/java/com/javassem/basic/MybatisTest.java
@@ -1,6 +1,6 @@
 package com.javassem.basic;
 
-import java.sql.Connection;
+
 
 import javax.sql.DataSource;
 
@@ -10,27 +10,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.extern.log4j.Log4j;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
 public class MybatisTest {
 
-	
+	@Autowired
 	private DataSource dataSource;
 	
-	
+	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
 	
-	
+	@Test
 	public void timeTest() {
 		try {
-		
 			SqlSession session = sqlSessionFactory.openSession();
-			Connection con = session.getConnection();
-			
-			
+			//Connection con = session.getConnection();
+			String result = session.selectOne("org.javassem.TestMapper.getTime");
+			log.info(result);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
